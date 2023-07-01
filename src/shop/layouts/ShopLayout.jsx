3f4abc8322e20570885944a37fe057febdded852
fml <lizarragafranco91@gmail.com>
2023-06-30 22:39:32 -0300

    minimal style change to shop layout

--- src/shop/layouts/ShopLayout.jsx
+++ src/shop/layouts/ShopLayout.jsx
@@ -1,49 +1,49 @@
 import { createStyles, Flex } from "@mantine/core";
-import { HeaderTabsColored } from ".."
+import { HeaderTabsColored } from "..";
 
-const useStyles = createStyles( theme => ({
+const useStyles = createStyles((theme) => ({
   root: {
-    position: 'absolute',
+    position: "absolute",
     padding: 0,
     margin: 0,
     minHeight: "100%",
     maxHeight: "100vp",
     width: "100%",
-    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0],
+    backgroundColor:
+      theme.colorScheme === "dark"
+        ? theme.colors.dark[8]
+        : theme.colors.gray[0],
   },
-  
+
   inner: {
     padding: 0,
     margin: 0,
-    position: 'relative',
-    height: '100%',
+    position: "relative",
+    height: "100%",
   },
 
   content: {
     margin: theme.spacing.md,
     marginTop: 120,
-    position: 'relative',
-  }
+    position: "relative",
+  },
+}));
 
-}) );
-
-export const ShopLayout = ({children}) => {
+export const ShopLayout = ({ children }) => {
   const { classes } = useStyles();
   return (
     <Flex
-      className={ classes.root }
+      className={classes.root}
       gap="md"
       justify="stretch"
       align="stretch"
       direction="column"
       wrap="wrap"
     >
-      <div className={classes.inner} >
-        <HeaderTabsColored tabs={['Home', 'Shop', 'About']} />
-        <div className={classes.content} >
-          { children }
-        </div>
+      <div className={classes.inner}>
+        <HeaderTabsColored tabs={["Home", "Shop", "About"]} />
+        <div className={classes.content}>{children}</div>
       </div>
     </Flex>
-  )
-}
+  );
+};
